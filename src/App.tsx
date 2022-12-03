80efb7ca474d164e6a47934e4ba7125caf74f60f
marouaneXan <marouanezahaoui835@gmail.com>
2022-12-03 13:05:53 +0100

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello world</h1>
     </div>
   )
 }
